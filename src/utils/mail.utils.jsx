f03a537669d14eb7c17b32cb70d1c47b5839cb1b
mustafa alkilani <mustafa@mustafa>
2023-02-13 17:35:12 +0300

    config some stuff

--- src/utils/mail.utils.jsx
+++ src/utils/mail.utils.jsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react';
 import { MailFormContext } from '../context/mail-form.context';
-import { MailServer } from '../../netlify/functions/send-mail';
+// import { MailServer } from '../../netlify/functions/send-mail';
 
 
 export const SendMail = async() => {
   const {inputFiledsValues} = useContext(MailFormContext);
   console.log(inputFiledsValues);
-  MailServer(inputFiledsValues);
+  // MailServer(inputFiledsValues);
 };
