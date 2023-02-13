1499fe51086723deb569efb1d23ddff9a760bb94
mustafa alkilani <mustafa@mustafa>
2023-02-13 18:04:10 +0300

    fixing mail

--- src/utils/mail.utils.jsx
+++ src/utils/mail.utils.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const SendEmail = async ({html}) => {
+export const SendEmail = async (html) => {
   try {
     const response = await axios.post('/send-email', { html });
     console.log(response.data);
